0200f48b1aa27fc8556048b70326b4f2061bf398
sahabulislamsifat <sahabulislamsifat@gmail.com>
2024-09-07 13:56:44 +0600

    arry.js

---
+++ arry.js
@@ -0,0 +1,69 @@
+// var name = "Sifat";
+// var age = 22;
+// var profession = "Student";
+// console.log(name, age, profession);
+
+// const numbers = [22, 33, 44, 55, 966, 77, 88, 99];
+// console.log(numbers)
+// console.log(numbers.length)
+//  console.log(numbers[0])
+// const x = numbers[1];
+// console.log(x)
+
+// numbers.push(100);
+// numbers.push(200);
+// console.log(numbers)
+
+// numbers.pop();
+// numbers.pop();
+// console.log(numbers)
+
+// const fruit = ['aam', 'jaam', 'kathal', 'lichu'];
+// const flower = ['shapla', 'golap', 'joba', 'hasnahena'];
+// const numbers = [22, 33, 44, 55, 66, 77, 88, 99];
+// const akakar = fruit.concat(flower);
+
+// console.log(fruit)
+// console.log(flower)
+// console.log(akakar)
+// console.log(numbers)
+
+// console.log(fruit[3])
+
+// const find = flower.indexOf('joba');
+// console.log(find)
+
+// console.log(flower.includes("golap"))
+// console.log(flower.includes("rojonigondha"))
+
+// const fools = fruit.join();
+// const fools = fruit.join("-");
+// const fools = fruit.join("/");
+// const fools = fruit.join("+");
+// console.log(fools)
+// console.log(typeof fools)
+
+// const string = "kacchi, biriyani, tehari, polao, khichuri";
+// const convert = string.split(",");
+// const convert = string.split("a");
+// console.log(convert)
+// console.log(Array.isArray(convert))
+
+// Slice
+// const flower = ['rose', 'lily', 'sunflower', 'beli', 'togor', 'gada'];
+
+// console.log(flower.slice(1,4))
+
+// SHIFT UNSHIFT
+// const flower = ['rose', 'lily', 'sunflower', 'beli', 'togor', 'gada'];
+// flower.shift()
+// flower.shift()
+// flower.shift()
+
+// console.log(flower)
+
+// flower.unshift('daliya');
+// flower.unshift('krishnochura');
+
+// console.log(flower)
+
